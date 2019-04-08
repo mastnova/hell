1b9f15962d8315211d2a652b15eb84916af276fb
Denis Matveev <mast747@bk.ru>
2019-04-09 00:31:29 +0300

    Add scroll down button

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-new fullpage('#fullpage', {
+const FP = new fullpage('#fullpage', {
   autoScrolling: true,
   navigation: true,
+});
+
+document.getElementById('scroll-down').addEventListener('click', function () {
+  fullpage_api.moveSectionDown();
 });
